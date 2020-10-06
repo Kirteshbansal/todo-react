665c100c699ce46d14f7c08990c7b23ab09881f5
Kirtesh Bansal <kirtesh.13@mountblue.tech>
2020-10-06 11:56:36 +0530

    Add functionality to display & delete projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
 
 class Projects extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
   render() {
-    const { addProject, stateData } = this.props;
+    const { addProject, stateData, onDelete, onSelect } = this.props;
+    const renderData = Object.entries(stateData["projectsData"]);
+    const displayProjects = renderData.map((p) => {
+      return (
+        <option key={p[0]} value={p[1]["projectName"]} data-projectid={p[0]}>
+          {p[1]["projectName"]}
+        </option>
+      );
+    });
+
     return (
       <div className="col-md-6 p-5 mx-auto my-3">
         <h2 className="text-white text-center p-2 text-300">My Projects</h2>
@@ -27,6 +31,27 @@ class Projects extends Component {
           </button>
         </form>
         <hr className="border-white" />
+        <div className="input-group">
+          <select
+            id="projects-list"
+            className="custom-select text-dark"
+            name="projects"
+            onChange={onSelect}
+          >
+            <option key="dummy" value="" hidden>
+              Select your project
+            </option>
+            {displayProjects}
+          </select>
+          <button
+            type="button"
+            className="btn btn-md btn-outline-danger ml-2 my-auto"
+            aria-label="delete project"
+            onClick={onDelete}
+          >
+            Delete
+          </button>
+        </div>
       </div>
     );
   }
